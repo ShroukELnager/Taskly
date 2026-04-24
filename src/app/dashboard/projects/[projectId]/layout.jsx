135de51dbba56d0beecbaab2b38a9d56da9a00e5
Shrouq Elnager <shrouk.kamel123@gmail.com>
2026-04-24 18:18:46 +0300

    feat(layout): add sidebar layout wrapper component

---
+++ src/app/dashboard/projects/[projectId]/layout.jsx
@@ -0,0 +1,15 @@
+
+export default function Sidebarlayout({ children }) {
+  return (
+    <div className="h-screen flex bg-[#F9F9FF] ">
+
+        <div className="flex-1 p-4 overflow-hidden">
+          {children}
+
+        </div>
+
+      </div>
+
+    
+  );
+}
