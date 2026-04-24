9ce091822e62e4610fa82fad792d3f2eb82779d9
Shrouq Elnager <shrouk.kamel123@gmail.com>
2026-04-24 18:11:44 +0300

    feat(redux): create StoreProvider with access token hydration

---
+++ src/app/StoreProvider.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useRef } from "react";
+import { Provider } from "react-redux";
+import { makeStore } from "./lib/store";
+import { setAccessToken } from "./lib/features/users/userSlice";
+
+export default function StoreProvider({ children, accessToken }) {
+  const storeRef = useRef(null);
+
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+
+    if (accessToken) {
+      storeRef.current.dispatch(setAccessToken(accessToken));
+    }
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
+}
