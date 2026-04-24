bb6a91d5dc241e67bba79096397b4a607adad77c
Shrouq Elnager <shrouk.kamel123@gmail.com>
2026-04-24 18:35:44 +0300

    feat(users): add fetchUsers async thunk with Supabase auth integration

---
+++ src/app/lib/features/users/usersThunk.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchUsers = createAsyncThunk(
+  "users/fetchUsers",
+  async (_, thunkAPI) => {
+    try {
+      const state = thunkAPI.getState();
+
+      const token = state?.users?.accessToken;
+
+      console.log("TOKEN:", token);
+
+      if (!token) {
+        throw new Error("No access token found");
+      }
+
+      const res = await fetch(
+        "https://pcufxstnppfqmzgslxlk.supabase.co/auth/v1/user",
+        {
+          method: "GET",
+          headers: {
+            apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!res.ok) {
+        const errorText = await res.text();
+        throw new Error(errorText || "Failed to fetch user");
+      }
+
+      return await res.json();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
