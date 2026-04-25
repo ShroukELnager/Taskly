1a4013db9b7af7ae8817e78edfb4d83dfa3a9497
Shrouq Elnager <shrouk.kamel123@gmail.com>
2026-04-25 06:28:21 +0300

    refactor: remove overflow hidden from sidebar layout

--- src/app/dashboard/projects/[projectId]/layout.jsx
+++ src/app/dashboard/projects/[projectId]/layout.jsx
@@ -3,7 +3,7 @@ export default function Sidebarlayout({ children }) {
   return (
     <div className="h-screen flex bg-[#F9F9FF] ">
 
-        <div className="flex-1 p-4 overflow-hidden">
+        <div className="flex-1 p-4 ">
           {children}
 
         </div>
