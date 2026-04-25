49eb75eb72023f33ada8244c925e523fa95ed0af
Shrouq Elnager <shrouk.kamel123@gmail.com>
2026-04-25 06:27:40 +0300

    feat: make dashboard layout responsive

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -3,35 +3,34 @@
 import Sidebar from "../components/sidebar";
 import Navbar from "../components/navbar";
 
-
-
-
-
 export default function Dashboardlayout({ children }) {
   return (
-    <div className="h-screen flex bg-[#F9F9FF] ">
+    <div className="min-h-screen bg-[#F9F9FF] flex justify-center">
 
-      {/* SIDEBAR */}
-      <div className=" h-full">
-        <Sidebar />
-      </div>
+      {/* CONTAINER */}
+      <div className="w-full max-w-[1440px] flex h-screen">
 
-      {/* RIGHT SIDE */}
-      <div className="flex flex-col flex-1 h-full ">
-
-        {/* NAVBAR */}
-        <div className="w-full">
-          <Navbar />
+        {/* SIDEBAR */}
+        <div className="h-full">
+          <Sidebar />
         </div>
 
-        {/* CONTENT */}
-        <div className="flex-1 p-4 overflow-auto ">
-          {children}
+        {/* RIGHT SIDE */}
+        <div className="flex flex-col flex-1 h-full">
+
+          {/* NAVBAR */}
+          <div className="w-full">
+            <Navbar />
+          </div>
+
+          {/* CONTENT */}
+          <div className="flex-1 p-4">
+            {children}
+          </div>
 
         </div>
 
       </div>
-
     </div>
   );
 }
